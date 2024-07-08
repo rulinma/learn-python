29283a213be8f5e2131f18365f7c677645b1dccf
rulinma@gmail.com <rulinma@gmail.com>
2024-07-08 12:13:17 +0800

    添加单元测试

--- test/unit/integration_test.py
+++ test/unit/integration_test.py
@@ -7,13 +7,6 @@ import sys
 
 if __name__ == '__main__':
     pass
-    # sys.path.append('/Users/rulin/program/temp/learn-python/')
-    # loader = unittest.TestLoader()
-    # # 假设你的测试文件都在test目录下，并且文件名以test_开头
-    # suite = loader.discover('./', pattern='test_*.py')
-    #
-    # runner = unittest.TextTestRunner(verbosity=2)
-    # runner.run(suite)
 
     # 运行测试
     # python -m unittest
