b10360e6ccad14a99f5ec5233c7adde74bfb5374
rulinma@gmail.com <rulinma@gmail.com>
2024-07-08 10:57:27 +0800

    添加单元测试

---
+++ test/unit/base/cls/test_employee.py
@@ -0,0 +1,24 @@
+import unittest
+
+from base.cls.employee import Employee
+from base.cls.parent import Child
+
+
+class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        print('setUp...')
+
+    def tearDown(self):
+        print('tearDown...')
+
+    def test_count(self):
+        emp1 = Employee("Zara", 2000)
+        # "创建 Employee 类的第二个对象"
+        emp2 = Employee("Manni", 5000)
+        emp1.display_employee()
+        emp2.display_employee()
+        self.assertEqual(2, emp2.empCount)
+
+
+if __name__ == '__main__':
+    unittest.main()
